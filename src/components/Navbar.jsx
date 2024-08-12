fbbc6666b5c284243e0dcd43de37c1f435e4ea79
sumitkandpal3 <kandpalsumit99@gmail.com>
2024-08-12 23:18:49 +0530

    anchor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">
-          <Link to="/">MyStore</Link> {/* Replace <a> with <Link> */}
+          <Link to="/">MyStore</Link> 
         </div>
         <div className="hidden md:flex space-x-4">
           <Link to="/" className="text-white hover:text-gray-300">
