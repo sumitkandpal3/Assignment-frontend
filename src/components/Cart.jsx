6f8cce00af65105765ec4d20e80988fbfea7834a
sumitkandpal3 <kandpalsumit99@gmail.com>
2024-08-13 07:21:38 +0530

    cart fixed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,20 +6,21 @@ const Cart = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        const { data } = await API.get("/cart/all");
-        if (data.success) {
-          setCart(data.cart);
-        }
-      } catch (error) {
-        setError(error.messsage);
-        setCart(null);
-      } finally {
-        setLoading(false);
+  const fetchCartItems = async () => {
+    try {
+      setLoading(true);
+      const { data } = await API.get("/cart/all");
+      if (data.success) {
+        setCart(data.cart);
       }
-    };
+    } catch (error) {
+      setError(error.messsage);
+      setCart(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
     fetchCartItems();
   }, []);
 
@@ -41,6 +42,7 @@ const Cart = () => {
       const { data } = await API.delete(`/cart/remove/${id}`);
       if (data.success) {
         setCart(data.cart);
+        fetchCartItems();
       } else {
         setError("Failed to remove from cart");
       }
@@ -52,7 +54,10 @@ const Cart = () => {
   const calculateTotal = () => {
     return cart?.items && cart.items.length > 0
       ? cart.items
-          .reduce((total, item) => total + item.product?.price * item.quantity, 0)
+          .reduce(
+            (total, item) => total + item.product?.price * item.quantity,
+            0
+          )
           .toFixed(2)
       : 0;
   };
@@ -71,85 +76,86 @@ const Cart = () => {
             <h2 className="text-xl font-semibold">Your Cart</h2>
           </div>
           <div className="p-4">
-            {!cart ? (
+            {loading ? (
+              <>loading...</>
+            ) : !cart ? (
               <p className="text-center text-gray-600">Your cart is empty.</p>
             ) : (
-              <ul>
-                {cart?.items.map((item) => (
-                  <li
-                    key={item.id}
-                    className="flex items-center border-b border-gray-300 py-4"
-                  >
-                    <img
-                      src={item.product?.thumbnail}
-                      alt={item.product?.name}
-                      className="w-24 h-24 object-cover rounded-md"
-                    />
-                    <div className="ml-4 flex-grow">
-                      <h3 className="text-lg font-semibold">
-                        {item.product?.name}
-                      </h3>
-                      <p className="text-gray-600">
-                        Price: ₹{item.product?.price}
-                      </p>
-                      <div className="flex items-center mt-2">
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, item.quantity - 1)
-                          }
-                          className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
-                          disabled={item.quantity <= 1}
-                        >
-                          -
-                        </button>
-                        <input
-                          type="number"
-                          min="1"
-                          value={item.quantity}
-                          onChange={(e) =>
-                            handleQuantityChange(
-                              item.id,
-                              parseInt(e.target.value)
-                            )
-                          }
-                          className="mx-2 w-16 text-center border border-gray-300 rounded"
-                        />
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, item.quantity + 1)
-                          }
-                          className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
-                        >
-                          +
-                        </button>
-                        <button
-                          onClick={() => handleRemoveItem(item._id)}
-                          className="ml-4 bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600"
-                        >
-                          Remove
-                        </button>
+              <>
+                <ul>
+                  {cart.items.map((item) => (
+                    <li
+                      key={item.id}
+                      className="flex items-center border-b border-gray-300 py-4"
+                    >
+                      <img
+                        src={item.product?.thumbnail}
+                        alt={item.product?.name}
+                        className="w-24 h-24 object-cover rounded-md"
+                      />
+                      <div className="ml-4 flex-grow">
+                        <h3 className="text-lg font-semibold">
+                          {item.product?.name}
+                        </h3>
+                        <p className="text-gray-600">
+                          Price: ₹{item.product?.price}
+                        </p>
+                        <div className="flex items-center mt-2">
+                          <button
+                            onClick={() =>
+                              handleQuantityChange(item._id, item.quantity - 1)
+                            }
+                            className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
+                            disabled={item.quantity <= 1}
+                          >
+                            -
+                          </button>
+                          <input
+                            type="number"
+                            min="1"
+                            value={item.quantity}
+                            onChange={(e) =>
+                              handleQuantityChange(
+                                item.id,
+                                parseInt(e.target.value)
+                              )
+                            }
+                            className="mx-2 w-16 text-center border border-gray-300 rounded"
+                          />
+                          <button
+                            onClick={() =>
+                              handleQuantityChange(item._id, item.quantity + 1)
+                            }
+                            className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600"
+                          >
+                            +
+                          </button>
+                          <button
+                            onClick={() => handleRemoveItem(item._id)}
+                            className="ml-4 bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600"
+                          >
+                            Remove
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+                    </li>
+                  ))}
+                </ul>
+                <div className="p-4 border-t border-gray-300">
+                  <div className="flex justify-between text-xl font-bold">
+                    <span>Total:</span>
+                    <span>₹{calculateTotal()}</span>
+                  </div>
+                  <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
+                    Checkout
+                  </button>
+                </div>
+              </>
             )}
           </div>
-          {cart && (
-            <div className="p-4 border-t border-gray-300">
-              <div className="flex justify-between text-xl font-bold">
-                <span>Total:</span>
-                <span>₹{calculateTotal()}</span>
-              </div>
-              <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
-                Checkout
-              </button>
-            </div>
-          )}
         </div>
       </section>
     </div>
   );
 };
-
 export default Cart;
